95844b71e9746345672f25748e8ff2fbe4a6ce0f
dobobaie <dobobaie@hotmail.fr>
2020-02-06 13:04:19 +0700

    Feature(Service): add online offline user status

---
+++ src/components/user/ActionsBottom.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-content class="userActionsBottom">
+    <div class="user-actions">
+      <v-btn class="logout-btn" block @click="() => $sModels.account.logout()">
+        <v-icon>mdi-logout</v-icon> <span v-if="$vuetify.breakpoint.smAndUp">{{ $_('LOGOUT') }}</span>
+      </v-btn>
+    </div>
+  </v-content>
+</template>
+
+<script>
+  export default {
+    name: 'UserActionsBottom'
+  };
+</script>
+
+<style scoped>
+  div.user-actions {
+    margin: 25px auto;
+  }
+  button.logout-btn {
+    background-color: #e36262 !important;
+    padding: 25px 35px;
+    color: #FFF;
+  }
+</style>
